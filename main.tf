9483f814a554e23e7c1fbb0d6f351513c5310331
Pawankumarmeena425 <pawankuamrmeena425@gmail.com>
2024-10-07 17:11:48 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "ap-south-1"  
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-078264b8ba71bc45e" 
+  instance_type = "t2.micro"              
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+
+}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
+}
